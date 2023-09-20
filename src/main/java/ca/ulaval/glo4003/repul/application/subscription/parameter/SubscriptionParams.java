270264c2e019ce4fad9eef4dec45ee1cef134969
Jonathan Bessette <JOBES10@ulaval.ca>
2023-09-20 14:51:30 -0400

    feat: create subscription params

--- src/main/java/ca/ulaval/glo4003/repul/application/subscription/parameter/SubscriptionParams.java
+++ src/main/java/ca/ulaval/glo4003/repul/application/subscription/parameter/SubscriptionParams.java
@@ -1,4 +1,13 @@
 package ca.ulaval.glo4003.repul.application.subscription.parameter;
 
-public record SubscriptionParams() {
+import ca.ulaval.glo4003.repul.domain.catalog.LocationId;
+
+public record SubscriptionParams(LocationId locationId) {
+    public SubscriptionParams(String locationId) {
+        this(new LocationId(locationId));
+    }
+
+    public static SubscriptionParams from(String locationId) {
+        return new SubscriptionParams(locationId);
+    }
 }
