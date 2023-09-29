0e6872c4dc9228b0c93fb0f271d9576807016730
Alexandre Mathieu <73568394+alexmathieu22@users.noreply.github.com>
2023-09-29 17:21:01 -0400

    feat: add demo to start batch (#124)

--- start.bat
+++ start.bat
@@ -1,2 +1,9 @@
-mvn clean verify
-java -jar target/application.jar
+if "%1"=="" (
+    echo "No arguments provided. Running Maven and Java without additional arguments."
+    mvn clean verify
+    java -jar target/application.jar
+) else (
+@REM     Run Maven and then run the Java application with the remaining arguments
+    mvn clean verify
+    java -jar target/application.jar "%1%"
+)
