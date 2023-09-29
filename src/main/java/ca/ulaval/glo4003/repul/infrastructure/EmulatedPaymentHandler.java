1929934de39a18325be76297c8a65ae3fd1ecc61
Christophe Duchesne <58085585+cduchesne1@users.noreply.github.com>
2023-09-29 17:21:12 -0400

    docs: update Postman collection (#123)

--- src/main/java/ca/ulaval/glo4003/repul/infrastructure/EmulatedPaymentHandler.java
+++ src/main/java/ca/ulaval/glo4003/repul/infrastructure/EmulatedPaymentHandler.java
@@ -3,12 +3,11 @@ package ca.ulaval.glo4003.repul.infrastructure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ca.ulaval.glo4003.config.DevApplicationContext;
 import ca.ulaval.glo4003.repul.domain.PaymentHandler;
 import ca.ulaval.glo4003.repul.domain.catalog.Amount;
 
 public class EmulatedPaymentHandler implements PaymentHandler {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DevApplicationContext.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmulatedPaymentHandler.class);
 
     @Override
     public Boolean makeTransaction(Amount amount) {
