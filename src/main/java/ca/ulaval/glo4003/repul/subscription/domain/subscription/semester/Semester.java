3c97db574f3eb1bb4aea4bf9bf7eb24ebf2fea47
Jonathan Bessette <74223159+JoBess7@users.noreply.github.com>
2023-12-15 02:59:02 -0500

    fix: semester to string (#477)
    Co-authored-by: Alexandre <alexaubut62@gmail.com>

--- src/main/java/ca/ulaval/glo4003/repul/subscription/domain/subscription/semester/Semester.java
+++ src/main/java/ca/ulaval/glo4003/repul/subscription/domain/subscription/semester/Semester.java
@@ -10,4 +10,9 @@ public record Semester(
     public boolean includes(LocalDate date) {
         return (date.isAfter(startDate) || date.isEqual(startDate)) && (date.isBefore(endDate) || date.isEqual(endDate));
     }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s - %s", semesterCode.value(), startDate.toString(), endDate.toString());
+    }
 }
